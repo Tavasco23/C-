15963494c37e3ef239a8549d40da125ec769cb7a
Tavasco23 <162991944+Tavasco23@users.noreply.github.com>
2025-07-20 13:12:34 +0800

    Add files via upload

--- Pointer.cpp
+++ Pointer.cpp
@@ -1,19 +1,17 @@
 #include <iostream>
 using namespace std;
 
-void displayValues(int* a, int* b , int* c) {
-    cout << "\nValues and memory addresses:\n";
-    cout << "Value of a = " << *a << ", Address = " << a << endl;
-    cout << "Value of b = " << *b << ", Address = " << b << endl;
-    cout << "Value of c = " << *c << ", Address = " << c << endl;
-}
-
 int main() {
-    int a, b, c;
+    int nums[3];
+    cout << "Enter 3 numbers: ";
     for (int i = 0; i < 3; ++i) {
-        cout << "Enter 3 numbers: ";
-        cin >> a >> b >> c;
-        displayValues(&a, &b, &c);
+        cin >> nums[i];
     }
+
+    cout << "\nValues and memory addresses:\n";
+    for (int i = 0; i < 3; ++i) {
+        cout << "Value " << i << " = " << *(nums + i) << ", Address = " << (nums + i) << endl;
+    }
+
     return 0;
 }
